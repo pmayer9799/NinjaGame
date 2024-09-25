53ee356e44de00b4e7b3535d05fab9f4588bd6ba
Patrick Mayer <patrick.mayer97@hotmail.com>
2024-09-25 14:07:30 -0700

    Step2: secretPaths

--- NinjaGame/Program.cs
+++ NinjaGame/Program.cs
@@ -1,21 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-using System.Reflection.Metadata.Ecma335;
-
-class NinjaGame
-
+﻿class NinjaGame
 {
-
     static char[,] _map;
-
     static int _ninjaX;
-
     static int _ninjaY;
-
     static int _shurikenCount = 3;//starting limit
+    static char _prevSecretPath = '\0';
 
     static void Main(string[] args)
     { //we will use the main loop to call the actual game to keep the Main() method clean
-        Load_map("map.txt");
+        Load_map("map2.txt");
         GameLoop();
     }
 
@@ -46,9 +39,10 @@ class NinjaGame
             Console.Clear();
             Print_map();//need to print map after each change
 
-            ThrowShuriken();//we always have the option to throw shuriken
+            ThrowShuriken();//we always have the option to throw shuriken if we have any
             Movement();
-
+            Console.Clear();
+            Print_map();//need to print map after each change
             Console.WriteLine("Count: " + _shurikenCount);
         }
     }
@@ -67,15 +61,50 @@ class NinjaGame
 
     static void MovePlayer(int newX, int newY)
     {
-        // Replace the player's old position with a space
-        _map[_ninjaX, _ninjaY] = ' ';
-
-        // Update the player's position
-        _ninjaX = newX;
-        _ninjaY = newY;
-
-        // Place the player in the new position
-        _map[_ninjaX, _ninjaY] = 'P';
+        switch(_map[newX, newY])
+        {
+            case '*':
+                _shurikenCount++;
+                Console.WriteLine(_shurikenCount);
+                _map[_ninjaX, _ninjaY] = 'P';
+                break;
+            case 'F':
+                SecretPath(newX, newY);                
+                break;
+            case 'G':
+                SecretPath(newX, newY);
+                break;
+            case 'H':
+                SecretPath(newX, newY);
+                break;
+            case 'I':
+                SecretPath(newX, newY);
+                break;
+            case 'j':
+                SecretPath(newX, newY);
+                break;
+            case 'K':
+                SecretPath(newX, newY);
+                break;
+            case 'L':
+                SecretPath(newX, newY);
+                break;
+            default:
+                // Replace the player's old position with a space if there was no secret path
+                if (_prevSecretPath != '\0')
+                {
+                    _map[_ninjaX, _ninjaY] = _prevSecretPath;
+                    _prevSecretPath = '\0';
+                }
+                else
+                    _map[_ninjaX, _ninjaY] = ' ';
+                //Update player position
+                _ninjaX = newX;
+                _ninjaY = newY;
+                // Place the player in the new position
+                _map[_ninjaX, _ninjaY] = 'P';
+                break;
+        }        
     }     
 
     static void ThrowShuriken()
@@ -91,17 +120,16 @@ class NinjaGame
                 {
                     if (_map[i, _ninjaY] == 'X')
                     {
-                        _map[i, _ninjaY] = '*'; // Remove $
+                        _map[i, _ninjaY] = '*';
                         _shurikenCount--;//decrease count if we hit a obstacle/symbol
                         Console.WriteLine("You hit the $ symbol below!");
                     }
                     else
                     {
-                        _map[i, _ninjaY] = ' '; // Remove $
+                        _map[i, _ninjaY] = ' '; // Remove $,X
                         _shurikenCount--;//decrease count if we hit a obstacle/symbol
                         Console.WriteLine("You hit the $ symbol below!");
                     }
-                    //break; // Stop checking further down
                 }
                 else if (_map[i, _ninjaY] == '#')
                     break; // Hit a wall, stop
@@ -120,11 +148,10 @@ class NinjaGame
                     }
                     else
                     {
-                        _map[_ninjaX, j] = ' '; // Remove $
+                        _map[_ninjaX, j] = ' ';//Remove $,X
                         _shurikenCount--;//decrease count if we hit a obstacle/symbol
                         Console.WriteLine("You hit the $ symbol to the right!");
                     }
-                    //break; // Stop checking further right
                 }
                 else if (_map[_ninjaX, j] == '#')
                     break; // Hit a wall, stop
@@ -137,17 +164,16 @@ class NinjaGame
                 {
                     if (_map[i, _ninjaY] == 'X')
                     {
-                        _map[i, _ninjaY] = '*'; // Remove $
+                        _map[i, _ninjaY] = '*';
                         _shurikenCount--;//decrease count if we hit a obstacle/symbol
                         Console.WriteLine("You hit the $ symbol above!");
                     }
                     else
                     {
-                        _map[i, _ninjaY] = ' '; // Remove $
+                        _map[i, _ninjaY] = ' '; // Remove $,X
                         _shurikenCount--;//decrease count if we hit a obstacle/symbol
                         Console.WriteLine("You hit the $ symbol above!");
                     }
-                    //break; // Stop checking further up
                 }
                 else if (_map[i, _ninjaY] == '#')
                     break; // Hit a wall, stop
@@ -160,17 +186,16 @@ class NinjaGame
                 {
                     if(_map[_ninjaX, j] == 'X')
                     {
-                        _map[_ninjaX, j] = '*'; // Remove $
+                        _map[_ninjaX, j] = '*';
                         _shurikenCount--;//decrease count if we hit a obstacle/symbol
                         Console.WriteLine("You hit the $ symbol to the left!");
                     }
                     else
                     {
-                        _map[_ninjaX, j] = ' '; // Remove $
+                        _map[_ninjaX, j] = ' '; // Remove $,X
                         _shurikenCount--;//decrease count if we hit a obstacle/symbol
                         Console.WriteLine("You hit the $ symbol to the left!");
                     }
-                    //break; // Stop checking further left
                 }
                 else if (_map[_ninjaX, j] == '#')
                     break; // Hit a wall, stop
@@ -183,13 +208,14 @@ class NinjaGame
         // Check for unbreakables
         return x >= 0 && x < _map.GetLength(0) &&
                y >= 0 && y < _map.GetLength(1) &&
-               _map[x, y] != '#';
+               _map[x, y] != '#' && _map[x, y] != 'X' &&
+               _map[x, y] != '$';
     }
 
     static void Movement()
     {
         // Try to move down
-        //IsValidMove checks if a # is in the way
+        //IsValidMove checks if a #,$,X is in the way
         if (IsValidMove(_ninjaX + 1, _ninjaY))
         {
             MovePlayer(_ninjaX + 1, _ninjaY);
@@ -211,6 +237,30 @@ class NinjaGame
         else if (IsValidMove(_ninjaX, _ninjaY - 1))
         {
             MovePlayer(_ninjaX, _ninjaY - 1);
+        }
+    }
+
+    static void SecretPath(int currX, int currY)
+    {        
+        // Find the other matching path to move to
+        for (int i = 0; i < _map.GetLength(0); i++)
+        {
+            for (int j = 0; j < _map.GetLength(1); j++)
+            {
+                if ((_map[i, j] == 'F' || _map[i, j] == 'G' || _map[i, j] == 'H' || _map[i, j] == 'I' || _map[i, j] == 'J' 
+                    || _map[i, j] == 'K' || _map[i, j] == 'L') && (i != currX || j != currY) && (_map[currX, currY] == _map[i,j]))
+                {                    
+                    if (_prevSecretPath != '\0')// need to check if prevSecretPath was not empty. we want to make sure to keep the original position of the path
+                        _map[_ninjaX, _ninjaY] = _prevSecretPath;
+                    else
+                        _map[_ninjaX, _ninjaY] = ' '; // Clear old position
+                    _prevSecretPath = _map[i, j];
+                    _ninjaX = i; // Update player's X position
+                    _ninjaY = j; // Update player's Y position
+                    _map[_ninjaX, _ninjaY] = 'P'; // Set new position
+                    return; // Exit once we move
+                }
+            }
         }
     }
 }
